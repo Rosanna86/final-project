8eae954007af5fd7b1a96ff63a7a526166f67b37
Jenny Quach <quach.jen@gmail.com>
2022-01-20 20:50:01 +0100

    endpoint signup works

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import crypto from "crypto"
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/finalproject"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -40,7 +42,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
+const authenticateUser = async (req, res, next) => { 
+	const accessToken = req.header('Authorization')
+
+	try {
+		const user = await User.findOne({ accessToken })
+		if (user) {
+      req.user = user
+			next()
+		} else {
+			res.status(401).json({ response: 'Please, log in', success: false })
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false })
+	}
+};
+
+// ROUTES
+
 app.get('/'), (req, res) => {
   res.send('Welcome to JKR')
 }
@@ -49,24 +68,26 @@ app.get('/my-pages', (req, res) => {
   res.send('Welcome')
 })
 
+
 app.post('/signup', async (req, res) => {
-  const { email, password } = req.body
+  const { name, email, password } = req.body
 
   try {
     const salt = bcrypt.genSaltSync()
 
     if (password.length < 8) {
-			throw 'Password must be at least 8 characters long';
+			throw 'Password must be at least 8 characters long'
     }
 
     const newUser = await new User({
+      name,
       email,
       password: bcrypt.hashSync(password, salt),
     }).save()
-
     res.status(201).json({
       response: {
         userId: newUser._id,
+        name: newUser.name,
         email: newUser.email,
         accessToken: newUser.accessToken
       },
@@ -76,6 +97,34 @@ app.post('/signup', async (req, res) => {
     res.status(400).json({ response: error, success: false})
   }
 })
+
+app.post('/signin', async (req, res) => {
+	const { email, password } = req.body
+
+	try {
+		const user = await User.findOne({ email })
+
+		if (user && bcrypt.compareSync(password, user.password)) {
+			res.status(200).json({ 
+				response: {
+					userId: user._id,
+					email: user.email,
+					accessToken: user.accessToken,
+				},
+				success: true,
+			})
+		} else {
+			res.status(404).json({
+				response: "Email or password doesn't match!",
+				success: false,
+			})
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false })
+	}
+});
+
+
 
 // Start the server
 app.listen(port, () => {
